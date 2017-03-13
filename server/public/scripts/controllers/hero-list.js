82cf62d4734f3a83f59b9ef6efd648e3408a8c03
Lopan455 <tjohander@gmail.com>
2017-03-13 13:43:16 -0500

    Hero Controller delete function working

--- server/public/scripts/controllers/hero-list.js
+++ server/public/scripts/controllers/hero-list.js
@@ -2,20 +2,30 @@ app.controller('HeroListController',['$http', function($http){
     console.log('Hero List Controller loaded');
     var self = this;
     self.testMessage = 'Test message is good';
-    getHeroes()
     self.heroesList = {};
+
+    getHeroes()
 
     function getHeroes(){
       $http({
         method: 'GET',
         url: '/heroes'
       }).then(function(response){
-        console.log('response from server is: ',response);
+        // console.log('response from server is: ',response);
          self.heroesList.list = response.data;
-
       })//end AJAX HTTP GET
     }//end function getHeroes()
 
+    self.deleteHero = function(id){
+      console.log('hit the deleteHeroes() function in the controller');
+      $http({
+        method: 'DELETE',
+        url: '/heroes/' + id
+      }).then(function(response){
+        console.log('deleteHeroes() complete. Response from server is: ',response);
+        getHeroes();
+      })
+    }
 
 
 
