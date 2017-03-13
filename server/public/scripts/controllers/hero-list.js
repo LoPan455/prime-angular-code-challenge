d8d773d928ad5331441f0b5f3d282062ebb551e0
Lopan455 <tjohander@gmail.com>
2017-03-13 13:35:25 -0500

    Heroes List table is populating

--- server/public/scripts/controllers/hero-list.js
+++ server/public/scripts/controllers/hero-list.js
@@ -1,3 +1,25 @@
-app.controller('HeroListController', function(){
+app.controller('HeroListController',['$http', function($http){
     console.log('Hero List Controller loaded');
-});
+    var self = this;
+    self.testMessage = 'Test message is good';
+    getHeroes()
+    self.heroesList = {};
+
+    function getHeroes(){
+      $http({
+        method: 'GET',
+        url: '/heroes'
+      }).then(function(response){
+        console.log('response from server is: ',response);
+         self.heroesList.list = response.data;
+
+      })//end AJAX HTTP GET
+    }//end function getHeroes()
+
+
+
+
+
+
+
+}]); //end controller
